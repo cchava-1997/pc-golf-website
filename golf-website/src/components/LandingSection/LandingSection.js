bb7adf1d6fec3394ea56eaf8f97bb6ad834c0b53
Prathima - Personal <carmelchava@gmail.com>
2024-11-28 11:16:05 -0800

    testing - code can be pushed from personal laptop

--- golf-website/src/components/LandingSection/LandingSection.js
+++ golf-website/src/components/LandingSection/LandingSection.js
@@ -11,7 +11,7 @@ const LandingSection = () => {
         </div>
       </div>
       <div className="landing-content">
-        <h1 className="landing-title">Where Luxury Meets Performance</h1>
+        <h1 className="landing-title">Where Luxury Meets Performance and perfection</h1>
         <p className="landing-description">
           Discover the perfect blend of luxury and efficiency with our premium golf products.
         </p>
