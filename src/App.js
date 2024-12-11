1a766431629aef12bc2774808424eda358ecf776
Prathima <carmelchava@gmail.com>
2024-12-11 00:03:08 -0800

    added public url in html.index

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import logo from './logo.svg';
-import './App.scss';
+import './App.css';
 import MainLayout from './layouts/MainLayout';
 import HomePage from './pages/HomePage';
 
 function App() {
   return (
-      <MainLayout>
-        <HomePage />
-      </MainLayout>
+      // <MainLayout>
+      //   <HomePage />
+      // </MainLayout>
+      <div>
+      <h1>Welcome to PC Golf Website</h1>
+      <p>Your app is working!</p>
+    </div>
     );
   }
 
